e7e1be2ce2df87b82e3b634caeb192ca2588b350
BennyLouie <bennylouie1412@gmail.com>
2019-10-24 13:16:14 -0400

    removed an incorrect validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :pokemon
   has_many :likes, dependent: :destroy
   validates :image, presence: true
-  validates :image, format: { with: %r{\.gif|jpg|png}i }
+  # validates :image, format: { with: %r{\.gif|jpg|png|jpeg}i }
   validates :caption, length: { maximum: 75 }
   # Uncomment the following for seeding unique images, comment out after seeding, only required for making each seed post image unique---
   # validates :image, :uniqueness => true
