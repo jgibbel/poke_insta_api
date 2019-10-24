fc1345bf263e9aa057ca545f8718ab8420aa26b3
BennyLouie <bennylouie1412@gmail.com>
2019-10-24 11:10:30 -0400

    added likes dependency

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :pokemon
-  has_many :likes
+  has_many :likes, dependent: :destroy
   # Comment out the following after seeding, only required for making each seed post image unique---
   validates :image, :uniqueness => true
 
