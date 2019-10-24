baec5854a52b17c519998b3dceea598090a8df01
BennyLouie <bennylouie1412@gmail.com>
2019-10-24 11:12:16 -0400

    added instructions to seed file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :pokemon
   has_many :likes, dependent: :destroy
-  # Comment out the following after seeding, only required for making each seed post image unique---
-  validates :image, :uniqueness => true
+  # Uncomment the following for seeding unique images, comment out after seeding, only required for making each seed post image unique---
+  # validates :image, :uniqueness => true
 
 
   def likes_count
